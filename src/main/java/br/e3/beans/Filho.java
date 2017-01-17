e9ff3c345fbcf69da6af5ae9ac2074602d800bd0
Erik Paixão <erikpaixao18@gmail.com>
2017-01-17 12:11:25 -0200

    Remoção classe AbstractPessoa

--- src/main/java/br/e3/beans/Filho.java
+++ src/main/java/br/e3/beans/Filho.java
@@ -6,17 +6,26 @@ import javax.persistence.Entity;
 import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
 
 import br.e3.beans.util.Permissao;
+import br.e3.util.BaseEntity;
 
 @Entity
 @AttributeOverride(name = "id", column = @Column(name = "pk_id"))
-public class Filho extends AbstractPessoa{
+public class Filho extends BaseEntity<Long>{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6323655942759291909L;
+	private String nome;
+	private String sobrenome;
+	private String password;
+	@Email @NotNull
+	private String email;
 	@ManyToOne
 	private Pai pai;
 	@OneToOne
@@ -27,6 +36,34 @@ public class Filho extends AbstractPessoa{
 	public Filho() {
 		super();
 	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setPermissao(Permissao permissao) {
+		this.permissao = permissao;
+	}
+	
 	public Pai getPai() {
 		return pai;
 	}
--- src/main/java/br/e3/beans/Pai.java
+++ src/main/java/br/e3/beans/Pai.java
@@ -7,23 +7,64 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
 
 import br.e3.beans.util.Permissao;
+import br.e3.util.BaseEntity;
 
 @Entity
 @AttributeOverride(name = "id", column = @Column(name = "pk_id"))
-public class Pai extends AbstractPessoa {
+public class Pai extends BaseEntity<Long> {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4928804748609940544L;
+	private String nome;
+	private String sobrenome;
+	private String password;
+	@Email @NotNull
+	private String email;
 	@Enumerated
 	private Permissao permissao;
 	@OneToMany
 	private Collection<Filho> filhos;
-	
+
 	public Pai() {
 		super();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public Permissao getPermissao() {
@@ -41,5 +82,5 @@ public class Pai extends AbstractPessoa {
 	public void setFilhos(Collection<Filho> filhos) {
 		this.filhos = filhos;
 	}
-	
+
 }
